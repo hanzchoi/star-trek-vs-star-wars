15a59cd1bb517f8862779383555af2298ff7fb7a
Hanz <personabe1984@gmail.com>
2019-05-05 14:35:52 -0400

    using class to render out the app and populating the element the old way. I will implement the new way using hooks and functional component

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
+The buttons have the following format "<choice> | <clicks>"
+The Star Wars button is initialized at 20, the Star Trek button is initialized at 40
+When a button is clicked, its counter increases by 1
+When a button that was previously clicked is clicked again, its counter decreases by 1
+Buttons have the css class name "button"
+A button that has not been clicked has the CSS class name "unselected"
+A button that has been clicked has the CSS class name "selected"
+It is not possible to select both choices: when clicking a button, if the other one is selected, it becomes unselected.
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+//import ChoiceContainer from './ChoiceContainer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+class App extends React.Component{
+
+  // Declare a new state variable, which we'll call "count"
+  //const [count, setCount] = useState(0);
+  //The Star Wars button is initialized at 20, the Star Trek button is initialized at 40
+  state = {clicked: false, starWarsCount: 20, starTrekCount: 40}
+
+  buttonHandler = () => {
+    console.log("Button was clicked");
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>Star Trek or Star Wars</h1>
+        <div>
+          <button onClick={() => this.buttonHandler()}>Star Wars | {this.state.starWarsCount}</button>
+        </div>
+        <div>
+          <button onClick={() => this.buttonHandler()}>Star Trek | {this.state.starTrekCount}</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+      //
+      // <button>button one </button>
+      // <button>button two</button>
+      // <p>You clicked {count} times</p>
+      // <button onClick={() => setCount(count + 1)}>
+      //   Click me
+      // </button>
